fe9254057052a454f8175ca5deb99ba5ffe8f01a
Jeff Maughan <geophray@gmail.com>
2020-05-13 13:20:58 -0700

    Update port to match Stephen's app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
