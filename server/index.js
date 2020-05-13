2e542e54e5e6f4a8e60cfbdf36adf363a913aee5
Jeff Maughan <geophray@gmail.com>
2020-05-12 17:42:12 -0700

    Add proxy endpoints middleware for each service

--- server/index.js
+++ server/index.js
@@ -7,15 +7,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/items', createProxyMiddleware({
+app.use('/api/items/:id', createProxyMiddleware({
+  target: 'http://127.0.0.1:3001/',
+  changeOrigin: true,
+}));
+
+app.use('/items/:id', createProxyMiddleware({
   target: 'http://127.0.0.1:3002/',
   changeOrigin: true,
 }));
-app.use('/api/related_products', createProxyMiddleware({
+
+app.use('/api/related_products/:id', createProxyMiddleware({
   target: 'http://127.0.0.1:3003/',
+  changeOrigin: true,
+}));
+
+app.use('/api/allreviews', createProxyMiddleware({
+  target: 'http://127.0.0.1:3004/',
   changeOrigin: true,
 }));
 
 app.listen(port, () => {
   console.log(`Proxy server listening on http://127.0.0.1:${port}`);
-})
+});
